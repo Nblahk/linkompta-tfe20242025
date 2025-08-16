4ccf144b9b6d88454ae52d8b750cde4cf9bf898b
Nblahk <ahakeu.nabil@hotmail.fr>
2025-08-17 01:06:41 +0200

    Urls des vues

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import UserListView
+from .views import (
+    AdminUserListView,
+    ComptableUserListView,
+    ClientUserDetailView,
+)
 
 urlpatterns = [
     path("", UserListView.as_view(), name="user-list"),
+    path("admin-list/", AdminUserListView.as_view(), name="admin-list"),
+    path("comptable-list/", ComptableUserListView.as_view(), name="comptable-list"),
+    path("client-detail/", ClientUserDetailView.as_view(), name="client-detail"),
 ]
